ec9381d098df00b4e125cce9adc11a37ea5a416f
mahi25soni <msdraj02@protonmail.com>
2024-10-24 16:07:02 +0530

    Refactor : Spacing changing

--- src/components/CarouselWrapper/CarouselWrapper.tsx
+++ src/components/CarouselWrapper/CarouselWrapper.tsx
@@ -35,9 +35,9 @@ const CarouselWrapper = () => {
         {slides.map((slide, index) => (
           <div
             key={index}
-            className={`absolute inset-0 flex items-center justify-center transition-opacity duration-700 ease-in-out ${
+            className={`absolute inset-0 top-40 flex items-center justify-center transition-opacity duration-700 ease-in-out ${
               activeSlide === index ? "opacity-100" : "opacity-0"
-            } xl:pl-[180px lg:grid lg:grid-cols-12 lg:pl-[100px] lg:pr-[20px]`}
+            } xl:pl-[180px lg:top-0 lg:grid lg:grid-cols-12 lg:pl-[100px] lg:pr-[20px]`}
           >
             <div className="flex h-[300px] w-[334px] flex-col items-start justify-start gap-6 rounded-32 border border-basic bg-white p-5 text-center lg:col-span-6 lg:h-max lg:w-[494px] lg:text-left xl:col-span-5">
               <div className="line-clamp-2 text-[32px] font-bold leading-[48px] lg:text-[48px] lg:leading-[72px]">
@@ -70,7 +70,7 @@ const CarouselWrapper = () => {
       {/* Previous Button */}
       <button
         type="button"
-        className="group absolute start-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+        className="group absolute start-0 top-24 z-30 flex h-full cursor-pointer items-center justify-center px-4 lg:top-0"
         onClick={handlePrev}
       >
         <span className="inline-flex h-10 w-10 items-center justify-center rounded-full border-2 border-light-range bg-white text-range-text hover:border-range-line">
@@ -96,7 +96,7 @@ const CarouselWrapper = () => {
       {/* Next Button */}
       <button
         type="button"
-        className="group absolute end-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
+        className="group absolute end-0 top-24 z-30 flex h-full cursor-pointer items-center justify-center px-4 lg:top-0"
         onClick={handleNext}
       >
         <span className="inline-flex h-10 w-10 items-center justify-center rounded-full border-2 border-light-range bg-white text-range-text hover:border-range-line">
