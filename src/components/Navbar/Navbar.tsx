8b9026d8bcdb7afc66790a5f581277c11459fcc5
mahi25soni <msdraj02@protonmail.com>
2024-10-24 18:28:09 +0530

    Refactor : Slight css changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   };
   return (
     <div className="fixed left-0 top-0 z-50 flex h-[125px] w-full items-center justify-between bg-white/25 p-8 text-black backdrop-blur-[64px]">
-      <div className="flex items-center justify-start gap-[58px]">
+      <div className="flex items-center justify-start gap-[30px] lg:gap-[58px]">
         <div className="relative">
           <Image
             src="/theLogo.png"
@@ -34,7 +34,7 @@ const Navbar = () => {
             <div
               key={index}
               onClick={() => handleSelectTab(tab)}
-              className={`cursor-pointer text-base  ${selectedTab === tab ? "font-bold" : "font-normal"}`}
+              className={`hidden cursor-pointer text-base lg:block ${selectedTab === tab ? "font-bold" : "font-normal"}`}
             >
               {tab}
             </div>
@@ -43,7 +43,7 @@ const Navbar = () => {
         <div></div>
       </div>
       <div className="relative h-8 w-8">
-        <Image src="/List.svg" fill alt="search" className="object-fill"/>
+        <Image src="/List.svg" fill alt="search" className="object-fill" />
       </div>
     </div>
   );
