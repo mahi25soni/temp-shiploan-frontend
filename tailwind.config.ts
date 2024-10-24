1d6754fc278aeedad379b8408ba54c950cf8ebe4
mahi25soni <msdraj02@protonmail.com>
2024-10-24 11:53:37 +0530

    Core : color config in css and tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      borderRadius : {
+        "32" : "32px",
+      },
+      backgroundColor : {
+        "light-skin" : "var(--bg-light-skin)",
+        "light-purple" : "var(--bg-light-purple)",
+        "light-green" : "var(--bg-light-green)",
+        "light-blue" : "var(--bg-light-blue)",
+      },
       colors: {
+        'yellow-orange': 'var(--yellow-orange)',
+        'light-black': 'var(--light-black)',
+        'range-line': 'var(--range-line)',
+        'range-text': 'var(--range-text)',
+        'light-range': 'var(--light-range)',
+        'light-gray': 'var(--light-gray)',
       },
     },
   },
