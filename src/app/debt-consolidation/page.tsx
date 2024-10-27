43752d4e856a0df476997332c9996b2200147cff
mahi25soni <msdraj02@protonmail.com>
2024-10-27 20:47:44 +0530

    Core : Debt consolidation page setup

--- src/app/debt-consolidation/page.tsx
+++ src/app/debt-consolidation/page.tsx
@@ -47,6 +47,9 @@ const DebtConsolidation = () => {
     <PageWrapper heading='How Debt Consolidation works' altText='Background Image' bgColor='#FDEFF0' mainImage='/money and pie chart.png' description='Lorem ipsum dolor sit amet consectetur. Semper sed malesuada quisque orci tincidunt lectus sollicitudin quam. Convallis in nisl odio enim arcu neque. Nulla ipsum venenatis volutpat eu. Venenatis nisi.'>
 
         <LoanCalculator InputDataList={InputRangeData} formik={formik}></LoanCalculator>
+ 
+       
+        
 
     </PageWrapper>
   )
--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -14,13 +14,13 @@ interface PageWrapperProps {
 const PageWrapper = (data: PageWrapperProps) => {
   return (
     <div
-      className="m-auto flex  lg:h-[1192px] h-[852px] w-full flex-row items-center justify-center"
+      className="m-auto flex  lg:h-[1192px] h-full w-full flex-row items-center justify-start"
       style={{
         background: `${data?.bgColor}`,
       }}
     >
-      <div className="flex w-full flex-col items-center gap-10 lg:flex-row lg:justify-center lg:gap-20">
-        <div className="relative h-[292px] w-[282px] lg:h-[544px] lg:w-[526px]">
+      <div className="flex w-full  flex-col items-center gap-10 justify-start  lg:flex-row lg:justify-center lg:gap-20">
+        <div className="relative min-h-[292px] w-[282px] lg:h-[544px] lg:w-[526px]">
           <Image
             src={data?.mainImage}
             layout="fill"
@@ -28,12 +28,13 @@ const PageWrapper = (data: PageWrapperProps) => {
             alt="hero-image"
           />
         </div>
-        <div className="flex h-full flex-col items-center px-10 gap-5 lg:w-[500px] lg:items-start">
+        <div className="justify-start flex h-full flex-col items-center px-10 gap-5 lg:w-[500px] lg:items-start">
           <MainHeading heading={data?.heading} align="left" />
           <div className="description-text-18">
             {data?.description}
           </div>
-          {data?.children}
+            {data?.children}
+          
         </div>
       </div>
     </div>
