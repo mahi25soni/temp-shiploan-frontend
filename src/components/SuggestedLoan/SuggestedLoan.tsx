96ea19943c58748d75e469e59f8f7d245442f1cf
mahi25soni <msdraj02@protonmail.com>
2024-10-28 09:39:26 +0530

    Core : Suggested loan components built

---
+++ src/components/SuggestedLoan/SuggestedLoan.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+interface SuggestedLoanProps {
+    amount : number,
+    tenure : number,
+    roi : number,
+}
+
+const SuggestedLoan = (data: SuggestedLoanProps) => {
+  return (
+    <div className="rounded-32 bg-white border-basic p-8 text-4xl backdrop-blur-md shadow-[0_4px_4px_rgba(0,0,0,0.25)] flex flex-col gap-5 w-[361px]">
+                <div className="text-light-gray font-semibold text-[18px] leading-[21.78px]">
+            Suggested Loan
+        </div>
+
+        <div className='border border-[#33333326] w-full'></div>
+
+        <div className='flex flex-col gap-4 w-full '>
+            <div className='flex justify-between items-center'>
+                <div className='text-sm leading-[16.94px] text-[#48454C]'>
+                    Amount
+                </div>
+                <div className='text-sm font-medium text-[#333333]'>
+                    {"Rs "}{data?.amount}
+                </div>
+            </div>
+            <div className='flex justify-between items-center'>
+                <div className='text-sm leading-[16.94px] text-[#48454C]'>
+                    Tenure
+                </div>
+                <div className='text-sm font-medium text-[#333333]'>
+                {data?.tenure}{' months'}
+                </div>
+            </div>
+            <div className='flex justify-between items-center'>
+                <div className='text-sm leading-[16.94px] text-[#48454C]'>
+                    Amount
+                </div>
+                <div className='text-sm font-medium text-[#333333]'>
+                    {data?.roi}{" %"}
+                </div>
+            </div>
+        </div>
+
+        <div className='border border-[#33333326] w-full'></div>
+
+        <div className='flex flex-col gap-4 '>
+        <button className="w-full rounded-32 bg-light-gray p-4 text-[18px] font-bold text-yellow-orange border border-light-gray  leading-[21.78px]">
+                Reduce Debt
+              </button>
+              <button className="w-full rounded-32 bg-transparent p-4 text-[18px] font-bold text-light-gray border border-light-gray  leading-[21.78px]">
+                Get full report
+                </button>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default SuggestedLoan
---
+++ src/components/wrappers/SuggestedLoanWrapper.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import React, { ReactNode } from 'react'
+
+interface SuggestedLoanWrapperProps {
+    bgColor: string;
+    mainImage: string;
+    altText: string;
+    children: ReactNode;
+  }
+
+const SuggestedLoanWrapper = (data: SuggestedLoanWrapperProps ) => {
+  return (
+    <div
+    className="m-auto flex  lg:h-[1192px] h-[852px] w-full flex-row items-center justify-start"
+    style={{
+      background: `${data?.bgColor}`,
+    }}
+  >
+    <div className="flex w-full  flex-col items-center gap-10 justify-start  lg:flex-row lg:justify-center lg:gap-20">
+      <div className="relative min-h-[292px] w-[282px] lg:h-[544px] lg:w-[526px]">
+        <Image
+          src={data?.mainImage}
+          layout="fill"
+          objectFit="fill"
+          alt={data?.altText}
+        />
+      </div>
+        {data?.children}
+    </div>
+  </div>
+  )
+}
+
+export default SuggestedLoanWrapper
