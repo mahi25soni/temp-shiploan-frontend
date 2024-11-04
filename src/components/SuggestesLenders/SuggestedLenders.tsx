b5c0bf226858ac860946faba525f81943641a160
mahi25soni <msdraj02@protonmail.com>
2024-11-04 17:18:41 +0530

    Refactor : Plus sign changing to black when selecting

--- src/components/SuggestesLenders/SuggestedLenders.tsx
+++ src/components/SuggestesLenders/SuggestedLenders.tsx
@@ -29,12 +29,20 @@ const SuggestedLenders = (data: props) => {
 
 
     const handleAddToCompare = (lender: LenderInfo) => {
-        console.log("this is the lender", lender)
-        if (firstLenderCandidate === null) {
-            setFirstLenderCandidate(lender)
+        const lenderId = lender.id;
+        const isAlreadyInComparison = data?.lenderComparisonArray.some(existingLender => existingLender.id === lenderId);
+
+
+        if (!isAlreadyInComparison && data?.lenderComparisonArray.length < 2) {
+            setSelectedLenderIds([...selectedLenderIds, lenderId]);
+            data.setLenderComparisonArray([...data?.lenderComparisonArray, lender]);
         }
-        else if (secondLenderCandidate === null) {
-            setSecondLenderCandidate(lender)
+        else if (isAlreadyInComparison) {
+            const updatedComparisonArray = data?.lenderComparisonArray.filter(existingLender => existingLender.id !== lenderId);
+            const updatedSelectedLenderIds = selectedLenderIds.filter(id => id !== lenderId);
+
+            data.setLenderComparisonArray(updatedComparisonArray);
+            setSelectedLenderIds(updatedSelectedLenderIds)
         }
     }
 
@@ -44,15 +52,15 @@ const SuggestedLenders = (data: props) => {
         const isAlreadyInComparison = data?.lenderComparisonArray.some(existingLender => existingLender.id === lenderId);
 
 
-        if(!isAlreadyInComparison && data?.lenderComparisonArray.length < 2){
+        if (!isAlreadyInComparison && data?.lenderComparisonArray.length < 2) {
             setSelectedLenderIds([...selectedLenderIds, lenderId]);
             data.setLenderComparisonArray([...data?.lenderComparisonArray, lender]);
             data.setLenderComparison(true);
         }
-        else if ( isAlreadyInComparison ) {
+        else if (isAlreadyInComparison) {
             const updatedComparisonArray = data?.lenderComparisonArray.filter(existingLender => existingLender.id !== lenderId);
             const updatedSelectedLenderIds = selectedLenderIds.filter(id => id !== lenderId);
-    
+
             data.setLenderComparisonArray(updatedComparisonArray);
             setSelectedLenderIds(updatedSelectedLenderIds)
         }
@@ -60,7 +68,7 @@ const SuggestedLenders = (data: props) => {
     }
 
     const compareLenders = () => {
-        data.setLenderComparisonArray([firstLenderCandidate, secondLenderCandidate])
+        console.log('Comparing Lenders')
         data.setLenderComparison(true)
     }
     return (
@@ -69,18 +77,18 @@ const SuggestedLenders = (data: props) => {
         >
 
             {
-                firstLenderCandidate && <div className='shadow-[0_2px_8px_rgba(0,0,0,0.25)] p-6 flex flex-col lg:hidden rounded-2xl w-[353px] bg-white gap-4 sticky top-[130px] left-0 z-10'>
+                data?.lenderComparisonArray.length > 0 && <div className='shadow-[0_2px_8px_rgba(0,0,0,0.25)] p-6 flex flex-col lg:hidden rounded-2xl w-[353px] bg-white gap-4 sticky top-[130px] left-0 z-10'>
                     <div className='flex items-center justify-around h-[29px]'>
                         <div className='description-text-24 font-bold text-center   '>
-                            {firstLenderCandidate?.["name"]}
+                            {data?.lenderComparisonArray?.[0]["name"]}
 
                         </div>
                         <BrownLine height={true} />
                         <div className='description-text-24 font-bold text-center'>
-                            {secondLenderCandidate ? secondLenderCandidate["name"] : <Image src='/PlusCircle.svg' width={18} height={18} alt="Add Button" className='rounded-full' />}
+                            {data?.lenderComparisonArray.length > 1 ? data?.lenderComparisonArray?.[1]["name"] : <Image src='/PlusCircle.svg' width={18} height={18} alt="Add Button" className='rounded-full' />}
                         </div>
                     </div>
-                    <button disabled={secondLenderCandidate ? false : true} onClick={compareLenders} className={` p-4 rounded-32 border   w-full description-text-18 font-bold  ${secondLenderCandidate ? 'border-light-gray text-light-gray' : 'border-gray-opacity-20 text-gray-opacity-20'}`}>
+                    <button disabled={data?.lenderComparisonArray.length === 2 ? false : true} onClick={compareLenders} className={` p-4 rounded-32 border   w-full description-text-18 font-bold  ${data?.lenderComparisonArray.length === 2 ? 'border-light-gray text-light-gray' : 'border-gray-opacity-20 text-gray-opacity-20'}`}>
                         Compare
                     </button>
                 </div>
@@ -103,7 +111,7 @@ const SuggestedLenders = (data: props) => {
 
             {data?.lenderArray.map((lender, index) => {
                 return (
-                    <SingleLenderInfo lender={lender} key={index} handleAddToCompare={handleAddToCompare} handleAddToCompareDesktop={handleAddToCompareDesktop} selectedLenderIds = {selectedLenderIds}/>
+                    <SingleLenderInfo lender={lender} key={index} handleAddToCompare={handleAddToCompare} handleAddToCompareDesktop={handleAddToCompareDesktop} selectedLenderIds={selectedLenderIds} />
                 )
             })}
 
@@ -122,7 +130,7 @@ const SingleLenderInfo = ({
     lender: LenderInfo,
     handleAddToCompare: (lender: LenderInfo) => void,
     handleAddToCompareDesktop: (lender: LenderInfo) => void
-    selectedLenderIds : string[]
+    selectedLenderIds: string[]
 
 }) => {
 
@@ -132,12 +140,12 @@ const SingleLenderInfo = ({
         <div className='p-6 bg-white rounded-32 flex justify-between items-center gap-4 w-[353px] shadow-[0_2px_8px_rgba(0,0,0,0.25)] lg:w-full lg:rounded-2xl lg:p-8 
     '>
             <div className='flex flex-col gap-4 lg:w-full'>
-                <div className='flex items-center gap-1 lg:h-[58px] lg:justify-between lg:w-full'>
-                    <div className='font-bold text-2xl leading-[29.05px] lg:text-[40px] lg:leading-[58px] '>
+                <div className='flex items-center  gap-1 lg:h-[58px] lg:justify-between lg:w-full'>
+                    <div className='font-bold text-2xl leading-[29.05px] lg:text-[40px] lg:leading-[58px] text-center '>
                         {lender?.name}
                     </div>
                     <div className='hidden lg:flex gap-4 '>
-                        <div  className={`flex justify-center items-center gap-3 p-4 description-text-18 rounded-32 cursor-pointer ${isSelected ? 'bg-black text-white' : 'bg-gray-opacity-5 text-black'}`} onClick={() => handleAddToCompareDesktop(lender)}>
+                        <div className={`flex justify-center items-center gap-3 p-4 description-text-18 rounded-32 cursor-pointer ${isSelected ? 'bg-black text-white' : 'bg-gray-opacity-5 text-black'}`} onClick={() => handleAddToCompareDesktop(lender)}>
                             <Image src='/PlusCircle.svg' width={18} height={18} alt="Add Button" className={`rounded-full ${isSelected ? "text-white bg-white" : "text-black"}`} />
                             <div>Add to Compare</div>
                         </div>
@@ -147,13 +155,13 @@ const SingleLenderInfo = ({
                             <div>Visit Lender Website</div>
                         </div>
                     </div>
-                    <div onClick={() => handleAddToCompare(lender)} className='lg:hidden'>
+                    <div onClick={() => handleAddToCompare(lender)} className={`lg:hidden h-6 w-6 relative`}>
 
-                        <Image src='/PlusCircle.svg' width={18} height={18} alt="Add Button" className='rounded-full' />
+                        <Image src='/PlusCircle.svg' fill alt="Add Button" className={`rounded-full ${isSelected ? "text-white bg-black" : "text-black "}`} />
                     </div>
-                    <div className='lg:hidden'>
+                    <div className='lg:hidden  h-6 w-6 relative'>
 
-                        <Image src='/connect.svg' width={18} height={18} alt="Connect Button" className='rounded-full' />
+                        <Image src='/connect.svg' fill alt="Connect Button" className={`rounded-full`} />
                     </div>
                 </div>
                 <div className='lg:hidden w-full '>
@@ -210,6 +218,7 @@ const SingleLenderInfo = ({
                         {lender?.eligibility}
                     </div>
                 </div>
+                <div className='h-[1px]'></div>
                 <div className='flex flex-col gap-1 '>
                     <div className='text-xs leading-[14.52px] text-[#000000]'>
                         Disbursement
