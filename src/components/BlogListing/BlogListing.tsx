6648eb8507a304b21861a202c9fa2c3fabb8738e
mahi25soni <msdraj02@protonmail.com>
2024-11-28 14:25:19 +0530

    refactor: update blog component structure and styling; sanitize content and improve variable naming

--- src/components/BlogListing/BlogListing.tsx
+++ src/components/BlogListing/BlogListing.tsx
@@ -5,35 +5,44 @@ import { BlogSampleData } from "@/testdata/blog-data";
 import Image from "next/image";
 import Link from 'next/link';
 import { useNavbar } from "@/context/contextApi";
+import axios from "../../../axios"
+import DOMPurify from 'dompurify';
+
 
 interface BlogItemInterface {
-  blog_title: string;
-  blog_content: string;
+  category: string;
+  title: string;
+  content: string;
   author: string;
-  date: string;
+  createdAt: string;
+  bannerImage: string
 }
 
-const data: BlogItemInterface[] = BlogSampleData;
+// const data: BlogItemInterface[] = BlogSampleData;
 
 const BlogItem = (props: BlogItemInterface) => {
+
+  function sanitizeContent(content: string) {
+    const sanitized = DOMPurify.sanitize(content, { FORBID_TAGS: ['img'] });
+    return sanitized;
+  }
   return (
     <div className="flex h-[423px] min-w-[317px] flex-col gap-[25px] pb-3">
-      <div className="relative h-[314px] w-full rounded-32 border-basic">
+      <div className="relative min-h-[314px] w-full rounded-32 border-basic">
         <Image
-          src="/blogbg1.svg"
+          src={props.bannerImage}
           alt="Blog Item Background"
           fill
-          className="object-fill"
+          className="object-fill rounded-32"
         ></Image>
         <div className="absolute bottom-6 px-4 text-[30px] font-bold leading-[45px] text-white line-clamp-3 ">
-          {props.blog_title}
+          {props.title}
         </div>
       </div>
-      <div className="line-clamp-2 text-[18px] font-medium leading-[22px]">
-        {props.blog_content}
+      <div className="overflow-hidden text-[18px] font-medium leading-[22px]  htmlBlogContent" dangerouslySetInnerHTML={{ __html: sanitizeContent(props?.content || '') }}>
       </div>
       <div className="text-sm font-normal leading-4">
-        {props.author} {" . "} {props.date}
+        {props.author} {" . "} {props.createdAt.split("T")[0]}
       </div>
     </div>
   );
@@ -41,26 +50,37 @@ const BlogItem = (props: BlogItemInterface) => {
 
 const BlogListing = () => {
   const { setSelectedTab } = useNavbar()
-  const [blogDataArray, setBlogDataArray] = useState<BlogItemInterface[]>(data);
+  const [blogDataArray, setBlogDataArray] = useState<BlogItemInterface[]>([]);
+
+
+
 
   useEffect(() => {
-    setBlogDataArray(data);
-  }, []);
+    (async () => {
+      const { data } = await axios.get(`/blog/get-all`);
+      setBlogDataArray(data?.data);
+    })()
+  }, [])
+
+
+
   return (
 
     <div className="lg:pl-[20px]">
       <MainHeading heading="Learn more ways to save" align="center" />
       <div className="mt-10 scrollbar-hidden flex flex-row w-full items-center justify-start gap-8 lg:gap-[50px] overflow-auto cursor-pointer pl-4 lg:pl-0">
         {blogDataArray?.map((blog, index) => {
-          const slug = blog.blog_title.toLowerCase().replace(/ /g, '-');
+          const slug = blog.title.toLowerCase().replace(/ /g, '-');
 
           return (
             <Link href={`/blog/${slug}`} key={index} onClick={() => setSelectedTab("Blog")}>
               <BlogItem
-                blog_title={blog.blog_title}
-                blog_content={blog.blog_content}
+                title={blog.title}
+                content={blog.content}
                 author={blog.author}
-                date={blog.date}
+                createdAt={blog.createdAt}
+                bannerImage={blog.bannerImage}
+                category={blog.category}
               />
             </Link>
           );
